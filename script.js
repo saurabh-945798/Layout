01f47a61eef376146d3bc9861fd438bac630a02e
Saurabh Sharma <132290433+saurabh-945798@users.noreply.github.com>
2024-02-07 21:30:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,59 @@
-const panels = document.querySelectorAll(".panel");
 
-panels.forEach((panel) => {
-    panel.addEventListener("click", () => {
-        removeActiveClasses();
-        panel.classList.add("active");
-    });
-});
+// SLIDER KA JAVASCRIPT
 
-function removeActiveClasses() {
-    panels.forEach((panel) => {
-        panel.classList.remove("active");
-    })
+let items = document.querySelectorAll('.slider .list .item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+let thumbnails = document.querySelectorAll('.thumbnail .item');
+
+// config param
+let countItem = items.length;
+let itemActive = 0;
+// event next click
+next.onclick = function(){
+    itemActive = itemActive + 1;
+    if(itemActive >= countItem){
+        itemActive = 0;
+    }
+    showSlider();
 }
+//event prev click
+prev.onclick = function(){
+    itemActive = itemActive - 1;
+    if(itemActive < 0){
+        itemActive = countItem - 1;
+    }
+    showSlider();
+}
+// auto run slider
+let refreshInterval = setInterval(() => {
+    next.click();
+}, 5000)
+function showSlider(){
+    // remove item active old
+    let itemActiveOld = document.querySelector('.slider .list .item.active');
+    let thumbnailActiveOld = document.querySelector('.thumbnail .item.active');
+    itemActiveOld.classList.remove('active');
+    thumbnailActiveOld.classList.remove('active');
+
+    // active new item
+    items[itemActive].classList.add('active');
+    thumbnails[itemActive].classList.add('active');
+
+    // clear auto time run slider
+    clearInterval(refreshInterval);
+    refreshInterval = setInterval(() => {
+        next.click();
+    }, 5000)
+}
+
+// click thumbnail
+thumbnails.forEach((thumbnail, index) => {
+    thumbnail.addEventListener('click', () => {
+        itemActive = index;
+        showSlider();
+    })
+})
 
 
 
@@ -65,3 +107,6 @@ gsap.fromTo(
   );
   
   
+
+
+
